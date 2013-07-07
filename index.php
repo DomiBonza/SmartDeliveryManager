5eaf2125c5284219d6b9efd13323b01a3615a37e
Dominique Naser Muñoz <choco.domi@gmail.com>
2013-07-07 18:12:43 -0400

    index

--- index.php
+++ index.php
@@ -3,5 +3,19 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Smart Delivery Manager</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/base.css">
+		<link rel="stylesheet" href="css/skeleton.css">
+		<link rel="stylesheet" href="css/layout.css">
 	</head>
+	<body>
+		<header id="wrap-header">
+			<div id="contenedor-logo"></div>
+			<div id="contenedor-login"></div>
+		</header>
+		<section id="imagenapp"></section>
+		<section id="quees"></section>
+		<section id="registro"></section>
+	</body>
 </html>
